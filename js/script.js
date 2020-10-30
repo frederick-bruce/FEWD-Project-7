112969bdd63255bcfb1a1ac348ceddb742ea33b0
frederick-bruce <69643077+frederick-bruce@users.noreply.github.com>
2020-10-30 14:17:37 -0400

    update js
    add social media secion
    add alert section to js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const alertBanner = document.getElementById("alert");
+
+
+  alertBanner.innerHTML = 
+  
+    <div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>7</strong> overdue tasks to complete </p>
+    <p class="alert-banner-close">x</p>
+    </div>
+  
+
+  ////alert bannner event listener to turn display off when clicked
+
+alertBanner.addEventListener('click', e => {
+  const element = e.target;
+  if (element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+  }
+});
